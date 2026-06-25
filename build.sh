b70a9b13cb762d9a576e3e5ffa9cb34bc708ee50
xyzrlee <cnrickylee@gmail.com>
2026-06-25 16:39:27 +0800

    upd

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+echo "POSTGRES_VERSION=${POSTGRES_VERSION}"
+echo "PATRONI_VERSION=${PATRONI_VERSION}"
+
 docker buildx build \
     --build-arg POSTGRES_VERSION=${POSTGRES_VERSION} \
     --build-arg PATRONI_VERSION=${PATRONI_VERSION} \
